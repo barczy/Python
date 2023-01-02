d518c48c795bcc79810942a27647674ae3ed52b1
barczy <35809076+barczy@users.noreply.github.com>
2023-01-02 11:51:45 +0100

    Update anagrams.py
    Add my function

--- anagrams.py
+++ anagrams.py
@@ -1,13 +1,22 @@
 
 # https://www.hackerrank.com/challenges/java-anagrams/problem
 # commit
+
+def my_function(a):
+    if a % 2 == 0:
+        print("Páros")
+    else:
+        print("Páratlan")
+
 print("Hello world! \n") 
+my_function(3)
 word1 = input("Add meg az első szót! ")
 word2 = input("Add meg a második szót! ")
 if (len(word1)!=len(word2)):
     print("Nem egyeznek\n")
 else:
     print("Egyeznek\n")
-print(word1[1])
 for x in range(len(word1)):
     print(word1[x])
+
+

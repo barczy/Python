820d0418c391546af15a43e25a5dfc4d8c291791
Barczikay Zsolt <zsolt.barczikay@otpbank.hu>
2025-12-13 20:04:22 +0100

    Add info

--- Python basic/2_datastructure.py
+++ Python basic/2_datastructure.py
@@ -4,6 +4,7 @@
 ##
 ## list
 ##
+############################
 print("-----------------")
 print("List")
 print("-----------------")
@@ -18,9 +19,11 @@ for task in to_do_list:
     print(f"-{task}")
 
 print(to_do_list)
+############################
 ##
 ## sets: collection of unique items, unordered
 ##
+############################
 print("-----------------")
 print("Set")
 print("-----------------")
@@ -45,3 +48,69 @@ text="This is a text. This is an example" ## kiszedni belőle az egyedi szavakat
 words=text.split()
 unique_word_set=set(words)
 print(unique_word_set)
+
+############################
+##
+## Tuples : ordered collections of items that are immutables (mint egy lista, rendezett elemek, megváltoztathatatlan)
+##
+############################
+print("-----------------")
+print("Tuples")
+print("-----------------")
+numbers = tuple([0,6,1,2,3,4,5,6,2])
+print(numbers)
+print(numbers[3]) # indexelt
+print(numbers[2:4]) # indexelt
+mixed = (1, "akarmi", 3.14)
+print(mixed)
+## operations: + (concatenation), * (megsokszorozza a halmazt ua elemekkel)
+## mixed[1]="Barmi", hibát dob, nem változtatható meg....
+print(numbers.count(2)) # hányszor van benne ez az elem
+print(numbers.index(6)) # mi az első előfordulása ennek az elemnek
+numbers2 = 0,6,1,5,6,7 # így is definiálható (packing)
+print(numbers2)
+first,*midle,last = numbers2 # szétszedhető változókba, unpacking
+print(first)
+print(midle)
+## nested list, tuples
+lst=[[1,2,3],[5,6]]
+print(lst)
+
+
+############################
+##
+## Dictionaries: unordered collection of items. Data in key-value pair
+##
+############################
+print("-----------------")
+print("Dictionaries")
+print("-----------------")
+student = {"name":"thomas", "age":15, "tmp": 17}
+print(student)
+print(student['name'])
+print(student.get("name"))
+## add, upddate, delete
+student["age"]=17
+student["city"]="Budapest"
+del student["tmp"]
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items()) #tuples formára hozza
+# shallow copy
+student_copy=student
+student_copy2=student.copy() # valós másolat
+student["name"]="Thomas2" # másik példány is változik!!! csak pointer van
+print(student_copy)
+print(student_copy2)
+for key, value in student.items():
+    print(f"{key} - {value}")
+# nested dictionaries
+students = {
+    "student1" : {"name" : "A"},
+    "student2" : {"name" : "B"}
+}
+print(students["student1"]["name"])
+# conditional dictionary comprehensions !!!!!
+squares = { x:x**2 for x in range(10) if x%2==1}
+print(squares)
---
+++ Python basic/3_functions.py
@@ -0,0 +1,12 @@
+############################ 
+## Functions
+############################
+
+def ftest(arguments=3): # default paraméter
+    """Function description"""
+    value=2*arguments
+    print(value)
+    return value
+
+ftest()
+ftest(2)

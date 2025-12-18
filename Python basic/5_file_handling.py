48391c4a46b1b05415cd7f6df0d95a46e134175d
Barczikay Zsolt <zsolt.barczikay@otpbank.hu>
2025-12-18 19:05:59 +0100

    Add info

---
+++ Python basic/5_file_handling.py
@@ -0,0 +1,32 @@
+############################ 
+## Files handling
+############################
+##
+## Open files
+##
+############################
+with open('example.txt','r') as file:
+    for line in file:
+        print(line)
+## vagy
+with open('example.txt','r') as file:
+    content = file.read()
+    print(content)
+## írás
+with open("example_2.txt","w") as file: # w - overwrite, a -append
+    file.write("Hello World\n")
+    file.write("End of file")
+
+## binary files
+data = b'\x00\x01\x02\x03'
+with open('example.bin',"wb") as file:
+    file.write(data)
+with open('example.bin',"rb") as file:
+    print(file.read())
+
+## write and read
+with open('example_2.txt','w+') as file:
+    file.write("first line\n")
+    file.write("second line\n")
+    file.seek(0) # file elejére viszi a curzort
+    print(file.read())

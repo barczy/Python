d0626cb6b53811e077afe31b85b93be74290d93f
Barczikay Zsolt <zsolt.barczikay@otpbank.hu>
2026-01-08 16:32:30 +0100

    add mlflow

--- mlflow_example.py
+++ mlflow_example.py
@@ -5,4 +5,8 @@ MLFlow telepítése után indítás : mlflow ui
 http://127.0.0.1:5000/
 """
 mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+mlflow.set_experiment('Check localhost connection')
 
+with mlflow.start_run():
+    mlflow.log_metric('test',1)
+    mlflow.log_metric('John',2)
